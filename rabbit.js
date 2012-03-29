93cc3ea93ab0d10edfdcb39fed961e0b5ae7fb24
ygXnm <ygXnm.xxx@gmail.com>
2012-03-29 12:01:39 +0900

    first commit

---
+++ rabbit.js
@@ -0,0 +1,13 @@
+﻿var month = 0;
+var number_of_pairs = 1;
+var before_number_of_pairs = 0;
+
+while(month < 12){
+	number_of_pairs = number_of_pairs + before_number_of_pairs;
+	month = month + 1;
+
+	console.log("month: " + month);
+	console.log("pairs: " + number_of_pairs);
+	console.log("before: " + before_number_of_pairs);
+
+}
